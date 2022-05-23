ac237598218810fe01bddbb033079f16447d52d3
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-05-23 16:48:34 +0600

    mongodb-conncete

--- index.js
+++ index.js
@@ -1,10 +1,61 @@
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkxov.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+  try {
+    await client.connect();
+
+    console.log('thsi is me')
+    
+   
+    
+  } finally {
+
+
+
+
+
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
